f61eb807f5d6741604f655222ca22eaf97d95433
James Eastham <j.eastham1993@gmail.com>
2021-09-08 11:48:33 +0100

    Add simple implementation of a GrpcClient and a REST API to run as a proxy

---
+++ src/JEasthamDev.Grpc.Orders/Services/OrderService.cs
@@ -0,0 +1,49 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) James Eastham
+// ------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using JEasthamDev.Orders;
+
+namespace JEasthamDev.Grpc.Orders.Services
+{
+	public class OrderService : JEasthamDev.Orders.Orders.OrdersBase
+	{
+		/// <inheritdoc />
+		public async override Task<GetOrderReply> GetOrder(GetOrderRequest request, ServerCallContext context)
+		{
+			return new GetOrderReply()
+			{
+				CustomerId = "test@test.com",
+				OrderDate = Timestamp.FromDateTime(DateTime.UtcNow),
+				OrderId = Guid.NewGuid().ToString()
+			};
+		}
+
+		/// <inheritdoc />
+		public async override Task<ListCustomerOrdersReply> ListCustomerOrders(GetCustomerOrdersRequest request, ServerCallContext context)
+		{
+			var orders = new List<GetOrderReply>(1)
+			{
+				new GetOrderReply()
+				{
+					CustomerId = "test@test.com",
+					OrderDate = Timestamp.FromDateTime(DateTime.UtcNow),
+					OrderId = Guid.NewGuid().ToString()
+				},
+			};
+			
+			return new ListCustomerOrdersReply()
+			{
+				Order =
+				{
+					orders 
+				}
+			};
+		}
+	}
+}
---
+++ src/JEasthamDev.Grpc.RestProxy/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using JEasthamDev.Orders;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JEasthamDev.Grpc.RestProxy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet("{customerId}/order/{orderId}")]
+        public async Task<IActionResult> GetSpecificCustomerOrder(string customerId, string orderId)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Orders.Orders.OrdersClient(channel);
+
+            var order = await client.GetOrderAsync(new GetOrderRequest()
+            {
+                OrderId = orderId
+            });
+
+            return this.Ok(order);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetCustomerOrders(string customerId)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Orders.Orders.OrdersClient(channel);
+
+            var orders = await client.ListCustomerOrdersAsync(new GetCustomerOrdersRequest()
+            {
+                CustomerId = customerId
+            });
+
+            return this.Ok(orders);
+        }
+    }
+}
